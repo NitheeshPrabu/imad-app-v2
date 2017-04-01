8151a58a722e0e4b56780e7fd748f23aeb8dd173
NitheeshPrabu <nitheeshprabu.s@gmail.com>
2017-04-01 12:13:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
     database: 'nitheeshprabu',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'db-nitheeshprabu-74256'
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
